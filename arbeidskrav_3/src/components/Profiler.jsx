1a284df3b4ac931808e9667edf96f629bcd03db1
KristofferHoff <kristoffer.hoff@me.com>
2025-04-11 20:11:54 +0200

    Lagt til interesser på profilkort

--- arbeidskrav_3/src/components/Profiler.jsx
+++ arbeidskrav_3/src/components/Profiler.jsx
@@ -24,6 +24,7 @@ export default function Profiler({name}){
             {console.log(name)}
           <section>
             {filteredMedlem.map((person) => (
+              <>
               <article
                 key={person._id}
                >
@@ -34,8 +35,24 @@ export default function Profiler({name}){
                 <h3>{person.medlemsnavn}</h3>
                 <a>{person.epost}</a>
               </article>
+              </>
             ))}
           </section>
+
+          <section>
+          {filteredMedlem.map((person) => (
+            <article key={person._id + "-interesser"}>
+              <h3>Interesser</h3>
+              {person.interesser && person.interesser.length > 0 && (
+                <ul>
+                  {person.interesser.map((interesse, index) => (
+                    <li key={index}>{interesse}</li>
+                  ))}
+                </ul>
+              )}
+            </article>
+          ))}
+        </section>
           <Arbeidslogg />
         </>
     )
