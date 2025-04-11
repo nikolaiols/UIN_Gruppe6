895d07a450242dd4e43b349d17c1b388d1190393
KristofferHoff <kristoffer.hoff@me.com>
2025-04-11 23:19:25 +0200

    oppdatering css

--- arbeidskrav_3/src/components/Profiler.jsx
+++ arbeidskrav_3/src/components/Profiler.jsx
@@ -23,10 +23,10 @@ export default function Profiler({name}){
              <h1>profiler</h1>
             {console.log(medlem.map((m)=>m.medlemsnavn))}
             {console.log(name)}
-          <section>
+          <section className="profil">
             {filteredMedlem.map((person) => (
               <>
-              <article
+              <article className="bilde"
                 key={person._id}
                >
                 <img
@@ -34,29 +34,26 @@ export default function Profiler({name}){
                   alt={person.medlemsnavn}
                 />
               </article>
-              <article>
+            <div className="profil-innhold">
+              <article className="profil-info">
               <h3>{person.medlemsnavn}</h3>
-              <h3>Om meg</h3>
               <p>{person.about}</p>
               </article>
+
+              <article className="interesser">
+                <h3>Interesser</h3>
+                {person.interesser && person.interesser.length > 0 && (
+                  <ul>
+                    {person.interesser.map((interesse, index) => (
+                      <li key={index}>{interesse}</li>
+                    ))}
+                  </ul>
+                )}
+              </article>
+              </div>
               </>
             ))}
           </section>
-
-          <section>
-          {filteredMedlem.map((person) => (
-            <article key={`${person._id} -interesser`}>
-              <h3>Interesser</h3>
-              {person.interesser && person.interesser.length > 0 && (
-                <ul>
-                  {person.interesser.map((interesse, index) => (
-                    <li key={index}>{interesse}</li>
-                  ))}
-                </ul>
-              )}
-            </article>
-          ))}
-        </section>
           <Arbeidslogg />
         </>
     )
